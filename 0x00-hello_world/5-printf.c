cfb5e693812b5958b1c6b7964e04bf09264ecbd8
root <missywhizzy@gmail.com>
2023-03-16 22:33:12 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
  * Return: Always 0.
 */
 
