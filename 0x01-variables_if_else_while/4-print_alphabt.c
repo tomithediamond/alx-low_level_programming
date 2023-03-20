4a8a241ac13216e1dc7a4fbeb90db0c336dd0b8f
root <missywhizzy@gmail.com>
2023-03-20 12:54:41 -0700

    e and q omitted

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints lowercase letters except q and e.
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+        char letter;
+
+        for (letter = 'a'; letter <= 'z' && letter != 'e' && letter != 'q'; letter++)
+                putchar(letter);
+        putchar('\n');
+        return (0);
+
