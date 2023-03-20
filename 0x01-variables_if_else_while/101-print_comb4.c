672c27ac382acd225db9d45875eb51ebee8ad493
root <missywhizzy@gmail.com>
2023-03-20 16:32:06 -0700

    three separate digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - prints different combinations of three digits.
+ * Return: 0.
+ */
+
+
+int main(void)
+
+{
+	int x, y, z;
+
+	for (x = '0'; x < '8'; x++)
+	{
+	for (y = '1'; y < '9'; y++)
+	{
+	for (z = '2'; z <= '9'; z++)
+	{
+		if (x != y && y != z && x != z)
+	{
+	putchar(x);
+	putchar(y);
+	putchar(z);
+	if (x != 7 && y != 8)
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
