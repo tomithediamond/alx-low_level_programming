e35b67114908a7f8a50df5b5ed5de53493936cc1
root <missywhizzy@gmail.com>
2023-03-20 11:20:53 -0700

    lower then upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints lower then upper.
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	char low;
+
+	while (low = 'a'; low <='z'; low++)
+		putchar(low);
+
+	while (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
+
+	Return (0);
+}
