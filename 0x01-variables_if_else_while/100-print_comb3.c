4ab089e22b99cd800d7a1c79d3414101e3704b13
root <missywhizzy@gmail.com>
2023-03-20 15:33:18 -0700

    two separate digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - prints all possible combination of two different digits.
+ * Return: Always 0.
+ */
+
+
+int main(void)
+
+{
+	int s, b;
+
+	for (s = '0'; s <= '8'; s++)
+	{
+	for (b = '1'; b <= '9'; b++)
+	{
+		if (b > s && s != 8 && b != 9)
+		{
+		putchar(s);
+		putchar(b);
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
