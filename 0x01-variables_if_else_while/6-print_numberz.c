8406cedfcc24679404ac427a324e7683e44552f1
root <missywhizzy@gmail.com>
2023-03-20 13:54:14 -0700

    using putchar to display numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all base 10 single digits from 0 using putchar.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+        int single;
+
+        for (single = '0'; single <= '9'; single++)
+                putchar(single);
+        putchar('\n');
+        return (0);
+}
