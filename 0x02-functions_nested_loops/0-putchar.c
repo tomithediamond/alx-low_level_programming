9331b1c47cc3cfde5ef8381bf865634441bed2ae
root <missywhizzy@gmail.com>
2023-03-21 03:34:14 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - prints _putchar followed by a new line.
+ * Return: 0.
+ */
+
+
+int main(void)
+
+{
+	char a;
+
+	for (a = "_putchar")
+		write('a');
+	write('\n');
+	return (0);
+}
