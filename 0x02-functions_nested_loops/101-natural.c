95467807999ae8417ec8f4b65564a52aab7baa71
root <missywhizzy@gmail.com>
2023-03-23 08:17:01 -0700

    multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the sum of all the natural numbers below 1024 (excluded)
+ * that are multiples of 3 or 5, followed by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int f, g = 0;
+
+	for (f < 1024; f++)
+	{
+		if (f % 3 = 0) || (z % 5 =0)
+		{
+			g += 0;
+		}
+
+		printf("%d\n", g);
+	}
+}
