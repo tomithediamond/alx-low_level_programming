aeca8dfd0b03294b6f47108738a3799b59738b73
root <missywhizzy@gmail.com>
2023-03-23 05:15:57 -0700

    a plus b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - performs addition operation on two integers.
+ * @d: the int to be added to e.
+ * @e: int to be added to d.
+ * Return: the result of the addition of d and e.
+ */
+
+
+int add(int d, int e)
+{
+	int b;
+
+	b = d + e;
+
+	return (b);
+}
