5455910b2cb1aab446830d03790c561ac81abc88
root <missywhizzy@gmail.com>
2023-03-24 21:03:48 -0700

    0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,17 +3,16 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9, followed by a new line.
- * Return: Always 0.
  */
 
 
 void print_numbers(void)
 {
-	int n;
+	char n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		_putchar("%d", n);
+		_putchar(n);
 	}
 	_putchar("\n");
 }
