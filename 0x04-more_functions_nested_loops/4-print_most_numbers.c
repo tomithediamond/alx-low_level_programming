1a4312fac1574b591a7c93b86279e54849f3ec18
root <missywhizzy@gmail.com>
2023-03-24 21:35:18 -0700

    no 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,8 @@
 
 
 /**
- * print_most_numbers - prints numbers from 0 to 9, excluding 2 and 4.
+ * print_most_numbers - prints num from 0 to 9, excluding 2 and 4.
+ * Return: void.
  */
 
 
@@ -12,7 +13,13 @@ void print_most_numbers(void)
 	for (k = 0; k <= 9; k++)
 	{
 		if (!(k == 2 || k == 4))
+		{
 			_putchar(k);
+		}
+		else
+		{
+			continue;
+		}
 	}
 	_putchar('\n');
 }
