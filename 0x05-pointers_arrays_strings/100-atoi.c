8d01260095594bc463dd4e76444bbcc76e9ca551
root <missywhizzy@gmail.com>
2023-03-28 13:30:14 -0700

    convert str to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,7 +5,7 @@
  * _atoi - converts a string to an integer
  * @s: string to be converted
  * Return: the integer converted
- * Return: 0 if no numbers in the string
+ * 0 if no numbers in the string
  */
 
 
@@ -13,8 +13,12 @@ int _atoi(char *s)
 {
 	int a, b, c, d, len, f;
 
-	a = 0; b = 0; c = 0;
-	d = 0; len = 0; f = 0;
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	len = 0;
+	f = 0;
 
 	while (s[len] != '\0')
 		len++;
@@ -27,7 +31,7 @@ int _atoi(char *s)
 			d = s[a] - '0';
 			if (b % 2)
 				d = -d;
-			c = c *10 + d;
+			c = c * 10 + d;
 			f = 1;
 			if (s[a + 1] < '0' || s[a + 1] > '9')
 				break;
