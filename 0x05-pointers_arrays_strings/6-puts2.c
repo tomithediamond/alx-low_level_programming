f1dcb834ccc582ae2bd6c11558e67646e6a083d5
root <missywhizzy@gmail.com>
2023-03-28 09:51:56 -0700

    every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,8 +2,9 @@
 
 
 /**
- * puts2 - a function that prints every other character of a string,
+ * puts2 - a function that prints every other character of a string, 
  * starting with the first character, followed by a new line.
+ * @str: the string
  */
 
 
