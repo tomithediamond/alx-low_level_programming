f4ea4c95677ca7baf0d964c5566c4aa0a4d725f2
root <missywhizzy@gmail.com>
2023-04-04 10:05:39 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,9 +2,9 @@
 
 
 /**
- * reverse_array - function that reverses the content of an array of integers.
- * @a: an array of integers
- * @n: the number of elements of the array
+ * reverse_array - reverses the content of an array of integers.
+ * @a: array of integers
+ * @n: number of elements of the array
  */
 
 
